81830b5867d17136cf9b2f436f202efe978caece
Leo St. Amour <leo.c.stamour@gmail.com>
2018-03-17 22:04:51 -0400

    Fixed bug in get-volume.sh

--- .system/get-volume.sh
+++ .system/get-volume.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-amixer get Master | awk -F'[\[\]]' '{if ($6 == "off") {print "Muted"; } else {print $2;}}' | tail -n 1
+STATUS=$(amixer get Master | grep -Po "\[o.+\]" | grep -Po "[a-z]+")
+if [[ "$STATUS" == "off" ]]
+then
+    echo "Muted"
+else
+    amixer get Master | grep -Po "\[\d+%\]" | grep -Po "\d+"
+fi
